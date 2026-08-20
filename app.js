09eb5d77857ae17af6cb91e4efdadb49eea51433
Yusuf Kasimov <yusuf@example.com>
2026-08-20 19:57:05 +0600

    update js

--- app.js
+++ app.js
@@ -80,8 +80,7 @@
 
 
 
-let day = document.querySelector(".day")
-let month = document.querySelector(".month")
+
 let year = document.querySelector(".year")
 let hour = document.querySelector(".hour")
 let minut = document.querySelector(".minut")
@@ -92,9 +91,7 @@ let secunt = document.querySelector(".secunt")
 const timeFuck = () =>{
     return setInterval(()=>{
         let date = new Date()
-        day.textContent = `${date.getDate()}.`
-        month.textContent = `${date.getMonth()+1}.`
-        year.textContent = `${date.getFullYear()}`
+        year.textContent = `${date.toLocaleDateString("es-US")}`
         hour.textContent = `${date.getHours()}:`
         minut.textContent = `${date.getMinutes()}:`
         secunt.textContent = `${date.getSeconds()}`
